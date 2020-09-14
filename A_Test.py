5df0cbfb1cb21cfd3a14bed0091578127712bd82
narayanacharya <narayan.acharya@gsa.gov>
2020-09-14 08:45:29 -0400

    init

---
+++ A_Test.py
@@ -0,0 +1,24 @@
+# print("narayan Acharya ")
+# num1= 5
+# num2= 10
+# name = "naryan Acharya "
+# age= "27"
+
+# print(num1+ num2)
+
+# print (name + age)
+
+name = raw_input("type your name ")
+print("hello " + name)
+
+age = raw_input("type your age")
+
+print(age)
+
+# casting 
+
+
+num1= int(raw_input("type first number "))
+num2= int(raw_input("type second number"))
+
+print(num1+ num2)
---
+++ B_Casting.py
@@ -0,0 +1,16 @@
+# casting 
+
+
+num1= int(raw_input("type first number "))
+num2= int(raw_input("type second number"))
+total= num1 + num2
+
+print(total)
+print("the total is " + str(total))
+
+num1= float(raw_input("type number "))
+num2 = float(raw_input("type second number "))
+total = num1 + num2
+
+print(total)
+print("the total number is " + str(total) )
